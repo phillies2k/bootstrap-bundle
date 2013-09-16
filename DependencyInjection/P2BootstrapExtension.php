d135e859ab9300a8b96b3d80170652b43c1d7e29
phillies2k <mostgreedy@gmail.com>
2013-09-16 13:02:03 +0200

    Update extension * Use extension config for theme_path * Rename renderVariablesLess to generateVariablesLess * Refactor generateBootstrapLess

--- DependencyInjection/P2BootstrapExtension.php
+++ DependencyInjection/P2BootstrapExtension.php
@@ -24,7 +24,6 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class P2BootstrapExtension extends Extension implements PrependExtensionInterface
 {
-
     /**
      * {@inheritDoc}
      */
@@ -35,9 +34,7 @@ class P2BootstrapExtension extends Extension implements PrependExtensionInterfac
     }
 
     /**
-     * Allow an extension to prepend the extension configurations.
-     *
-     * @param ContainerBuilder $container
+     * {@inheritDoc}
      */
     public function prepend(ContainerBuilder $container)
     {
@@ -59,81 +56,49 @@ class P2BootstrapExtension extends Extension implements PrependExtensionInterfac
 
     protected function generateThemes(ContainerBuilder $container)
     {
-        $assetPath = $container->getParameter('kernel.root_dir') . '/Resources/less/themes';
+        $configs = $container->getExtensionConfig('P2BootstrapBundle');
+        $config = $this->processConfiguration(new Configuration(), $configs);
 
         foreach ($container->findTaggedServiceIds('bootstrap.theme') as $id => $attributes) {
             /** @var Theme $theme */
             $theme = $container->get($id);
 
-            $themePath = $assetPath . '/' . $theme->getName();
+            $themePath = $config['theme_path'] . '/' . $theme->getName();
 
             if (! is_dir($themePath)) {
                 mkdir($themePath, 0777, true);
             }
 
             if (! file_exists($themePath . '/bootstrap.less')) {
-                file_put_contents($themePath . '/bootstrap.less', $this->generateBootstrapLess());
+                file_put_contents($themePath . '/bootstrap.less', $this->generateBootstrapLess($container));
             }
 
             if (! file_exists($themePath . '/layout.less')) {
                 file_put_contents($themePath . '/layout.less', $this->generateLayoutLess($theme));
             }
 
-            file_put_contents($themePath . '/variables.less', $this->renderVariablesLess($theme));
+            file_put_contents($themePath . '/variables.less', $this->generateVariablesLess($theme));
 
-            $themeConfig = array();
-
-            $themeConfig['theme_' . $theme->getName()] = array(
-                'inputs' => $themePath . '/layout.less',
-                'output' => 'themes/' . $theme->getName() . '/style.css',
-                'filter' => array('less')
+            $container->prependExtensionConfig(
+                'assetic',
+                array(
+                    'assets' => $this->getAsseticThemeConfig($config, $theme)
+                )
             );
-
-            $container->prependExtensionConfig('assetic', array('assets' => $themeConfig));
         }
     }
 
-    protected function generateBootstrapLess()
+    protected function getAsseticThemeConfig(array $config, ThemeInterface $theme)
     {
+        $themeConfig = array();
 
+        $themeConfig['theme_' . $theme->getName()] = array(
+            'inputs' => $config['theme_path'] . '/layout.less',
+            'output' => 'themes/' . $theme->getName() . '/style.css',
+            'filter' => array('less')
+        );
 
-        $path = '../../../../../vendor/twitter/bootstrap/less/';
-        $filepath =  __DIR__ . '/' . $path . 'bootstrap.less';
-
-        $pattern = '/@import\s"([^"]+)";/';
-        $imports = array();
-
-        if (false === $count = preg_match_all($pattern, file_get_contents($filepath), $matches)) {
-            throw new \RuntimeException('preg_match_all encountered an error');
-        }
-
-        $template = "@import \"%s%s\";";
-        for ($i = 0; $i < $count; $i++) {
-            $imports[] = sprintf($template, $path, $matches[1][$i]);
-        }
-
-        $offset = array_search($path . 'variables.less', $imports) + 1;
-        array_splice($imports, $offset, 0, "@import \"variables.less\";");
-
-        $contents = "// This file is auto generated.\n\n";
-        $contents.= implode("\n", $imports);
-
-        return $contents;
-    }
-
-    protected function generateLayoutLess(ThemeInterface $theme)
-    {
-        return <<<LESS_LAYOUT
-// Theme: {$theme->getName()}
-//
-// Imports
-// -------------------------------------------------
-@import "bootstrap.less";
-
-// Put your custom styles here
-
-LESS_LAYOUT;
-
+        return $themeConfig;
     }
 
     protected function getThemeVariables(ThemeInterface $theme)
@@ -195,7 +160,48 @@ LESS_LAYOUT;
         return $variables;
     }
 
-    protected function renderVariablesLess(ThemeInterface $theme)
+    protected function generateBootstrapLess(ContainerBuilder $container)
+    {
+        $lessPath = $container->getParameter('kernel.root_dir') . '/../vendor/twitter/bootstrap/less';
+        $filePath = $lessPath . '/bootstrap.less';
+
+        $pattern = '/@import\s"([^"]+)";/';
+        $imports = array();
+
+        if (false === $count = preg_match_all($pattern, file_get_contents($filePath), $matches)) {
+            throw new \RuntimeException('preg_match_all encountered an error');
+        }
+
+        $template = "@import \"%s%s\";";
+        for ($i = 0; $i < $count; $i++) {
+            $imports[] = sprintf($template, $lessPath . '/', $matches[1][$i]);
+        }
+
+        $offset = array_search($lessPath . '/variables.less', $imports) + 1;
+        array_splice($imports, $offset, 0, "@import \"variables.less\";");
+
+        $contents = "// This file is auto generated.\n\n";
+        $contents.= implode("\n", $imports);
+
+        return $contents;
+    }
+
+    protected function generateLayoutLess(ThemeInterface $theme)
+    {
+        return <<<LESS_LAYOUT
+// Theme: {$theme->getName()}
+//
+// Imports
+// -------------------------------------------------
+@import "bootstrap.less";
+
+// Put your custom styles here
+
+LESS_LAYOUT;
+
+    }
+
+    protected function generateVariablesLess(ThemeInterface $theme)
     {
         $contents = "";
         foreach ($this->getThemeVariables($theme) as $name => $value) {
