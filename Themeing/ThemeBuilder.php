19ac0768fac95bf3096c426a94b17d71621cbec3
phillies2k <mostgreedy@gmail.com>
2013-09-18 12:40:12 +0200

    Fix path error in theme builder

--- Themeing/ThemeBuilder.php
+++ Themeing/ThemeBuilder.php
@@ -140,7 +140,7 @@ LESS_THEME;
      */
     protected function buildBootstrapVariables(ThemeInterface $theme)
     {
-        $variables = $this->parseVariablesFromFile($this->sourceDirectory . '/less');
+        $variables = $this->parseVariablesFromFile($this->sourceDirectory . '/less/variables.less');
 
         // we alter bootstraps font path, cause we're storing fonts general in themes/fonts instead of providing
         // the same files within every theme directory (e.g. themes/default/fonts)
