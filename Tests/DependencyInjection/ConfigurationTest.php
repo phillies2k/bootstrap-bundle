c41c10524cd7592de5011a6364940e69189e213e
phillies2k <mostgreedy@gmail.com>
2013-09-19 11:27:45 +0200

    Fix configuration test

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -28,12 +28,24 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
         $configuration = new Configuration(array());
         $config = $processor->processConfiguration($configuration, array(array()));
 
+        $this->assertArrayHasKey('use_themes', $config);
+        $this->assertArrayHasKey('public_path', $config);
         $this->assertArrayHasKey('jquery_path', $config);
         $this->assertArrayHasKey('source_path', $config);
         $this->assertArrayHasKey('themes_path', $config);
         $this->assertArrayHasKey('bootstrap_css', $config);
         $this->assertArrayHasKey('bootstrap_js', $config);
-        $this->assertArrayHasKey('jquery_js', $config);
         $this->assertArrayHasKey('holder_js', $config);
+        $this->assertArrayHasKey('jquery_js', $config);
+
+        $this->assertTrue($config['use_themes']);
+        $this->assertEquals('%kernel.root_dir%/../web/themes', $config['public_path']);
+        $this->assertEquals('%kernel.root_dir%/../components/jquery/jquery.js', $config['jquery_path']);
+        $this->assertEquals('%kernel.root_dir%/../vendor/twitter/bootstrap', $config['source_path']);
+        $this->assertEquals('%kernel.root_dir%/Resources/themes', $config['themes_path']);
+        $this->assertEquals('css/bootstrap.css', $config['bootstrap_css']);
+        $this->assertEquals('js/bootstrap.js', $config['bootstrap_js']);
+        $this->assertEquals('js/holder.js', $config['holder_js']);
+        $this->assertEquals('js/jquery.js', $config['jquery_js']);
     }
 }
